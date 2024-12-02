d53ec63da363d1df60730d195cfd0538ac9bebff
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2024-12-02 09:20:22 +0100

    Add files via upload

---
+++ DonneesStructurees/Tuples/exercices_tuple.py
@@ -0,0 +1,18 @@
+def combien(liste, element):
+    rep = 0
+    for i in range(len(liste)):
+        if element == liste[i]:
+            rep = rep +1
+    return rep        
+
+def produit(nombres,n):
+    rep = ()
+    for i in range(len(nombres)):
+        rep = rep + (nombres[i]*n,)
+    return rep 
+
+def tri_pair_impair(nombres):
+    pass
+
+def melange(tuple1,tuple2):
+    pass
