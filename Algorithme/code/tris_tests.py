8beb24c96f877220892c7baf392a1ebdd8ae3b0e
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2025-06-02 09:36:35 +0200

    Update tris_tests.py

--- Algorithme/code/tris_tests.py
+++ Algorithme/code/tris_tests.py
@@ -19,5 +19,30 @@ def tri_insertions(tab):
         j += 1
     return tab
 
+def tri_selection(tab):
+    '''renvoie un tableau de nombres entiers trié
+    tab => tableau d'entiers.
+    >>> tri_selection([6,5,4,8,2,1])
+    [1, 2, 4, 5, 6, 8]
+    >>> tri_selection([0,5,4,8,2,1])
+    [0, 1, 2, 4, 5, 8]
+    '''
+    i=0
+    while i< len(tab)-1:
+        j = i+1
+        mini = i
+        while j<len(tab):
+            if tab[j]<tab[mini]:
+                mini =j
+            j = j+1
+        if mini != i:
+            a = tab[i]
+            tab[i]=tab[mini]
+            tab[mini]= a
+        i=i+1
+    return tab
+
+    
+
 if __name__=="__main__":
     doctest.testmod(verbose=True)
