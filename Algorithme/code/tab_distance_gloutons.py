23c99932f0e1904007346ec2fd400f248bf1074d
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2025-06-03 08:59:45 +0200

    Add files via upload

---
+++ Algorithme/code/tab_distance_gloutons.py
@@ -0,0 +1,5 @@
+distance_tab = {"Nancy":{"Metz":55,"Paris":303,"Reims":188,"Troyes":183},
+                "Metz":{"Nancy":55,"Paris":306,"Reims":176,"Troyes":203},
+                "Paris":{"Nancy":303,"Metz":306,"Reims":142,"Troyes":153},
+                "Reims":{"Nancy":188,"Metz":176,"Paris":142,"Troyes":123},
+                "Troyes":{"Nancy":183,"Metz":203,"Paris":153,"Reims":123}}
