d49c96557057c35bf2374bb7b96058b417a10b7b
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2025-11-12 10:39:45 +0100

    Update exercices_tuple.py

--- DonneesStructurees/Tuples/exercices_tuple.py
+++ DonneesStructurees/Tuples/exercices_tuple.py
@@ -1,25 +1,11 @@
 def combien(liste, element):
-    rep = 0
-    for i in range(len(liste)):
-        if element == liste[i]:
-            rep = rep +1
-    return rep        
+    pass       
 
 def produit(nombres,n):
-    rep = ()
-    for i in range(len(nombres)):
-        rep = rep + (nombres[i]*n,)
-    return rep 
+    pass
 
 def tri_pair_impair(nombres):
-    nb_pair=()
-    nb_impair=()
-    for i in range(len(nombres)):
-        if nombres[i]%2==0:
-            nb_pair = nb_pair + (nombres[i],)
-        else:
-            nb_impair=nb_impair+(nombres[i],)
-    return (nb_pair,nb_impair)
+    pass
 
 def melange(tuple1,tuple2):
     pass
