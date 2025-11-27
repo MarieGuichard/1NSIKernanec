f8e1dd2a1c7e75abb2a288963f0da49f5e595874
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2025-11-27 08:20:01 +0100

    Update exercices_tuple.py

--- DonneesStructurees/Tuples/exercices_tuple.py
+++ DonneesStructurees/Tuples/exercices_tuple.py
@@ -5,7 +5,14 @@ def produit(nombres,n):
     pass
 
 def tri_pair_impair(nombres):
-    pass
+    pair = ()
+    impair = ()
+    for i in range(len(nombres)):
+        if nombres[i]%2 == 0:
+            pair = pair + (nombres[i],)
+        else:
+            impair = impair + (nombres[i],)
+    return pair,impair
 
 def melange(tuple1,tuple2):
     pass
