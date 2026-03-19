e7ea322aa487e863e13b71c331a6bb29c85894ec
MarieGuichard <79110007+MarieGuichard@users.noreply.github.com>
2026-03-19 09:10:53 +0100

    Update exercices_dictionnaires.py

--- DonneesStructurees/Dictionnaires/exercices_dictionnaires.py
+++ DonneesStructurees/Dictionnaires/exercices_dictionnaires.py
@@ -1,30 +1,28 @@
 magasin_A={"Pomme":10,"Poire":15,"Fraise":3,"Banane":7,"Cerise":10}
 magasin_B={"Pomme":3,"Poire":15,"Fraise":10,"Banane":15,"Cerise":4}
 
-
-def leplus(mag_A,mag_B):
-    magasin={}
-    #cle=mag_A.keys() # recupere toutes les clés du dictionnaire mag_A
-    for elt in mag_A.keys(): # boucle sur les clés du dictionnaire mag_A
-        if mag_A[elt]>mag_B[elt]:
-            magasin[elt]="mag_A"
-        elif mag_A[elt]< mag_B[elt]:
-            magasin[elt]="mag_B"
+def legrandmagasin(mag_A,mag_B):
+    grand_mag = {}
+    for cle_A in mag_A.keys():
+        if mag_A[cle_A]<mag_B[cle_A]:
+            grand_mag[cle_A] = "Magasin_B"
+        elif mag_A[cle_A]>mag_B[cle_A]:
+            grand_mag[cle_A] = "Magasin_A"
         else:
-            magasin[elt]="mag_A et mag_B"
-    return magasin
-
-
-print(leplus(magasin_A,magasin_B))
+            grand_mag[cle_A] =("Magasin_A", "Magasin_B")
+    return grand_mag
 
 eleve={"Nom":"Dupont","Prenom":"Jules","Age":17,"Classe":"1G4"}
-stagiaire={"Nom":"Dupont","Prenom":"Jules","Fonction":"Developpeur","Salaire":2500}
+stagiaire={"Nom":"Dupont","Prenom":"Jules","Fonction":"Developpeur","Salaire":2500}           
+        
+def fusion(dico1,dico2):
+    reponse = dico1
+    for cle in dico2.keys():
+        if cle not in reponse.keys():
+            reponse[cle] = dico2[cle]
+    return reponse
 
-def fusion(dic1,dic2):
-    reponse=dic1
-    for elt in dic2.keys():
-        if elt not in reponse:
-            reponse[elt]=dic2[elt]
+
     return reponse
 
 print(fusion(eleve,stagiaire))
